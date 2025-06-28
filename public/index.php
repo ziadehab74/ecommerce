7e8aab1234ce33236ca357cfc0c07428cc8ed8a3
ziadehab74 <zehab028@gmail.com>
2025-06-28 04:20:35 +0300

    Remove debugging statement from index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-dd(213123);
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
